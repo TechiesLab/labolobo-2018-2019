46591fe81043f419e125ef5ae1f8c45d019c9cc7
Margotte <margo.tinawi@gmail.com>
2019-07-07 20:08:10 +0200

    Initial commit

---
+++ louis/nav.js
@@ -0,0 +1,21 @@
+
+const navbar = document.querySelector
+('.topnav');
+console.log(navbar);
+const banner = document.querySelector
+(".banner");
+
+setTimeout(function(){
+	console.log(navbar);
+	navbar.classList.remove('anime');
+	banner.classList.add("after-effect")
+}, 3000)
+
+
+
+
+
+
+
+
+	
